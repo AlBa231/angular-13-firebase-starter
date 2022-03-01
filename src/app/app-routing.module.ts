9eabe9e2f0a1c9924f0cd6fc9a916b471834bd5d
Alexander Barabanov <Oleksandr_Barabanov@epam.com>
2022-03-01 15:35:21 +0200

    feat(home): added Dashboard page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
