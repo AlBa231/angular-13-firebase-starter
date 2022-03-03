45b3f478e574c23f9597458bec3e21f74440eed5
Alexander Barabanov <Oleksandr_Barabanov@epam.com>
2022-03-03 13:32:30 +0200

    feat(account): added template for account pages

--- src/app/modules/account/account-routing.module.ts
+++ src/app/modules/account/account-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
         path: '',
-        redirectTo: 'login',
-        pathMatch: 'full',
+        component: TemplateComponent,
+        children: [
+          {
+            path: 'login',
+            component: LoginComponent,
+          },
+          {
+            path: '',
+            redirectTo: 'login',
+            pathMatch: 'full',
+          },
+        ],
       },
     ]),
   ],
--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { LoginComponent } from './pages/login/login.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    TemplateComponent
   ],
   imports: [CommonModule, AccountRoutingModule],
 })
